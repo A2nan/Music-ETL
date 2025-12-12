c3192eb073edb91bde4a366b723f6e66fe9cdae1
Adnan <adnan@la-loupe.immo>
2025-12-12 09:02:15 +0100

    mise au propre backend

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/opensound"
+SQLALCHEMY_DATABASE_URL = "postgresql://uvanz7hdjlhcwfps7lh7:gRro38Ko8kBCvlBaz3TTf2HPBd6jTz@bhlvoyopvbuuym4bjiim-postgresql.services.clever-cloud.com:5432/bhlvoyopvbuuym4bjiim"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
