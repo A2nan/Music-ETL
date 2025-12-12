db2205f4e55c60c033cc237775054d1eaa58d928
pascal <pascala456@gmail.com>
2025-12-12 15:55:01 +0100

    Ajout AirFlow

---
+++ Airflow/dags/music_etl_pipeline.py
@@ -0,0 +1,30 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator  
+from datetime import datetime
+import sys
+
+
+sys.path.append("/opt/airflow/Backend")
+
+
+from app.services.etl_service import run_etl  
+
+def run_music_etl():
+    run_etl(genre="pop")   
+
+with DAG(
+    dag_id="music_etl_pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule="*/1 * * * *",   # Toutes les minutes
+    catchup=False,
+    tags=["music", "etl", "deezer"],
+) as dag:
+
+    etl_task = PythonOperator(
+        task_id="run_music_etl",
+        python_callable=run_music_etl,
+    )
+
+    etl_task
+
+
