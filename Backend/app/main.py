8251ce81f26c2f7c7270ee7da945a81dd4f4640f
Adnan <adnan@la-loupe.immo>
2025-12-12 09:34:42 +0100

    autorisation cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # ✅ IMPORT MANQUANT
 from app.routes.etl_routes import router as etl_router
 from app.database import engine, Base
 
@@ -8,6 +9,16 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(etl_router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",  # Vite
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],        # GET, POST, etc.
+    allow_headers=["*"],        # Content-Type, Authorization, etc.
+)
 
 @app.get("/")
 def read_root():
